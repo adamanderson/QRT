Fixed averaging on welch module

software/gnuRadio/gr-QRT/python/welch.py
@@ -69,11 +69,11 @@ class welch(gr.sync_block):
             
                 out[i] = pw
             '''
-            avg = np.add(np.zeros(self.nData),np.zeros(self.nData)*1.j)
-            for a in np.arange(avgn):
+            avg = numpy.add(numpy.zeros(self.nData),numpy.zeros(self.nData)*1.j)
+            for a in numpy.arange(avgn):
                 low = a*self.nData
                 high = (a+1)*self.nData
-                avg = np.add(avg,in0[low:high])
+                avg = numpy.add(avg,in0[low:high])
             avg = avg/nData
             f, pw = sp.welch(avg,fs=self.fs,
                     window='hann',nperseg=self.nf,