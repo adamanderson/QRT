91c09a2cdab84799797e506632cf6a01f5d0dbc6
Ryan Thornton <ryanthornton@ryanthontonsmbp.dhcp.fnal.gov>
2016-07-26 16:23:41 -0500

    Made RA & DEC sole perameters for motorcontrol

--- software/motors/MotorController.py
+++ software/motors/MotorController.py
@@ -30,12 +30,13 @@ class MotorControl(object):
         GPIO.output(18, GPIO.LOW)
         GPIO.output(24, GPIO.LOW)
 
-    def motorcontrol(self, ra, dec, dj2000): #creates the function to go to a set length, note: the values have to be in decimal form
+    def motorcontrol(self, ra, dec): #creates the function to go to a set length, note: the values have to be in decimal form
 	latitude = 41.825
 	longitude = -88.2439
 	radegs = ra * 15
     unitime = datetime.utcnow()
-    ut = unitime.hour() + unitime.minute()/60+unitime.second()/3600
+    ut = unitime.hour + unitime.minute/60+unitime.second/3600
+    dj2000 = float((calendar.timegm(time.gemtime()) - 946684800)/86400)
         lst = 100.46 + 0.985647 * dj2000 + longitude + (15*ut)
         while lst < 0:
             lst += 360
