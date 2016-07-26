84511b1f5a6c106937bdff91cd0c1f5d09c93e20
Ryan Thornton <ryanthornton@ryanthontonsmbp.dhcp.fnal.gov>
2016-07-26 16:49:49 -0500

    Fixed indentation and J2000

--- software/motors/MotorController.py
+++ software/motors/MotorController.py
@@ -32,12 +32,12 @@ class MotorControl(object):
         GPIO.output(24, GPIO.LOW)
 
     def motorcontrol(self, ra, dec): #creates the function to go to a set length, note: the values have to be in decimal form
-	latitude = 41.825
-	longitude = -88.2439
-	radegs = ra * 15
-    unitime = datetime.utcnow()
-    ut = unitime.hour + unitime.minute/60.+unitime.second/3600.
-    dj2000 = float((calendar.timegm(time.gmtime()) - 946727936.)/86400.)
+        latitude = 41.825
+        longitude = -88.2439
+        radegs = ra * 15
+        unitime = datetime.utcnow()
+        ut = unitime.hour + unitime.minute/60.+unitime.second/3600.
+        dj2000 = float((calendar.timegm(time.gmtime()) - 946727936.)/86400.)
         lst = 100.46 + 0.985647 * dj2000 + longitude + (15*ut)
         while lst < 0:
             lst += 360
