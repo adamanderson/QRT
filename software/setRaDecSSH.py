15e067169c7d5de37abb91db6670357005a19ea2
lucasmcdonald3 <lucasmcdonald3@gmail.com>
2016-08-02 12:28:05 -0500

    Hour/Minute/Second conversions done client-side

--- software/setRaDecSSH.py
+++ software/setRaDecSSH.py
@@ -1,24 +1,15 @@
 import sys
 import Pyro4
 
-raH = sys.argv[1]
-raM = sys.argv[2]
-raS = sys.argv[3]
-decH = sys.argv[4]
-decM = sys.argv[5]
-decS = sys.argv[6]
+ra = sys.argv[0]
+dec = sys.argv[1]
 
-function = sys.argv[7]
-
-raM += raS/60
-raH += raM/60
-decM += decS/60
-decH += decM/60
+function = sys.argv[2]
 
 proxy = Pyro4.core.Proxy("PYRONAME:motorcontroller.server")
 if(float(function) == 1):
-    proxy.motorcontrol(float(raH), float(decH))
+    proxy.motorcontrol(float(ra), float(dec))
 elif(float(function) == 2):
     proxy.reset()
 elif(float(function) == 3):
-    proxy.motorScan(float(raH), float(decH))
+    proxy.motorScan(float(ra), float(dec))
